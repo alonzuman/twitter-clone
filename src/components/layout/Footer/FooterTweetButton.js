c70bc00debfa3597a09f032ff83c3a9bd67c2bc5
Alon Zuman <alonzuman7@gmail.com>
2020-12-01 15:45:25 +0200

    finalized chat

--- src/components/layout/Footer/FooterTweetButton.js
+++ src/components/layout/Footer/FooterTweetButton.js
@@ -1,17 +1,32 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './FooterTweetButton.css';
 import AddIcon from '../../../assets/icons/AddIcon'
 import PrimaryButton from '../../buttons/PrimaryButton/PrimaryButton'
 import { TweetsContext } from '../../../contexts/TweetsContext';
+import { useHistory } from 'react-router-dom';
 
 const FooterTweetButton = () => {
+  const [display, setDisplay] = useState(false);
   const { openNewTweetDialog } = useContext(TweetsContext);
+  const history = useHistory();
 
-  return (
-    <PrimaryButton onClick={openNewTweetDialog} size='lg' className='footerTweetButton'>
-      <AddIcon className='footerTweetButton__icon' size={24} />
-    </PrimaryButton>
-  )
+  history.listen((location) => {
+    if (location.pathname === '/messages') {
+      setDisplay(false);
+    } else {
+      setDisplay(true);
+    }
+  })
+
+  if (display) {
+    return (
+      <PrimaryButton onClick={openNewTweetDialog} size='lg' className='footerTweetButton'>
+        <AddIcon className='footerTweetButton__icon' size={24} />
+      </PrimaryButton>
+    )
+  } else {
+    return null;
+  }
 }
 
 export default FooterTweetButton
